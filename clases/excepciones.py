95f688a376fa2a2857b82df8e2377dff6328d54c
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2022-04-03 15:48:33 +0200

    clases

---
+++ clases/excepciones.py
@@ -0,0 +1,25 @@
+import re
+
+class email:
+    def correo(self, s):
+        if re.search("@", s) != None:
+            if s.count('@') != 1 or s.count('.') !=1:
+                print('Cuenta bloqueada a causa de un ataque')
+            else:
+                posicion_arroba = re.search("@", s).start()
+                posicion_punto = re.search(".", s).start()
+                lista_s = list(s)
+                if posicion_punto > posicion_arroba:
+                    print('Cuenta bloqueada a causa de un ataque')
+                else:
+                    if lista_s[len(lista_s)-1] == '.':
+                        print('Cuenta bloqueada a causa de un ataque')
+                    else:
+                        nombre = []
+                        for i in range(posicion_arroba):
+                            nombre.append(lista_s[i])
+                        nombre = ''.join(nombre)
+                        print('¡Bienvenido', nombre,'!')
+        else:
+            print('Una dirección de correo electrónico debe tener el formato xxx@xxx.xx')
+            self.correo(input("Escriba su direccion de correo electronico: "))
---
+++ main.py
@@ -0,0 +1,5 @@
+from clases.excepciones import *
+
+if __name__ == '__main__':
+    ejercicio = email()
+    ejercicio.correo(input("Escriba su direccion de correo electronico: "))
